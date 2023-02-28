6c86240518a7d920dbece99683639fb50e50591c
Alex-prog814 <aleks.98.x.28@gmail.com>
2023-02-28 13:21:59 +0600

    Added finally commit

--- src/components/products/ProductCard/ProductCard.jsx
+++ src/components/products/ProductCard/ProductCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useProducts } from '../../../contexts/ProductContextProvider';
+import { useCart } from '../../../contexts/CartContextProvider';
+import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
+import IconButton from '@mui/material/IconButton';
 
 const ProductCard = ({ item }) => {
   const navigate = useNavigate();
   const { deleteProduct } = useProducts();
+  const { addProductToCart, checkProductInCart } = useCart();
 
   return (
     <div>
@@ -12,6 +16,10 @@ const ProductCard = ({ item }) => {
       <button onClick={() => navigate(`/details/${item.id}`)}>Details</button>
       <button onClick={() => navigate(`/edit/${item.id}`)}>Edit</button>
       <button onClick={() => deleteProduct(item.id)}>Delete</button>
+
+      <IconButton size="small" onClick={() => addProductToCart(item)}>
+        <AddShoppingCartOutlinedIcon color={checkProductInCart(item.id) ? "primary" : ""} />
+      </IconButton>
     </div>
   )
 }
